16f5ef77893cabc85c407648c930971c7d1b9b96
kartarsinghdebugged <163403168+kartarsinghdebugged@users.noreply.github.com>
2024-03-14 15:13:17 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+if (typeof window !== 'undefined') {
+  gsap.registerPlugin(ScrollTrigger, useGSAP);
+}
+
+export default function Home() {
+  const main = useRef<HTMLElement | any>();
+
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray('.box');
+      boxes.forEach((box) => {
+        gsap.to(box, {
+          x: 150,
+          scrollTrigger: {
+            trigger: box,
+            start: 'bottom bottom',
+            end: 'top 20%',
+            scrub: true,
+            // markers: true,
+          },
+        });
+      });
+    },
+    { scope: main }
+  );
+
+  return (
+    <div>
+      <section className="section flex-center column">
+        <h2>Basic ScrollTrigger with React</h2>
+        <p>Scroll down to see the magic happen!!</p>
+      </section>
+      <div className="section flex-center column" ref={main}>
+        <div className="box gradient-blue">box</div>
+        <div className="box gradient-blue">box</div>
+        <div className="box gradient-blue">box</div>
+      </div>
+      <section className="section"></section>
+    </div>
+  );
+}
